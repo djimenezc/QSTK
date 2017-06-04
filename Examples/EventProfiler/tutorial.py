c5d8f5592969120ef53d49afb9c1995dbf6face7
david jimenez <david.jimenez19@gmail.com>
2017-06-04 20:00:57 +0200

    format source code

--- Examples/EventProfiler/tutorial.py
+++ Examples/EventProfiler/tutorial.py
@@ -11,8 +11,7 @@ Created on March, 5, 2012
 @summary: Event Profiler Tutorial
 '''
 
-
-import pandas 
+import pandas
 from qstkutil import DataAccess as da
 import numpy as np
 import math
@@ -40,54 +39,55 @@ nan = no information about any event.
 """
 
 # Get the data from the data store
-storename = "Yahoo" # get data from our daily prices source
+storename = "Yahoo"  # get data from our daily prices source
 # Available field names: open, close, high, low, close, actual_close, volume
 closefield = "close"
 volumefield = "volume"
 window = 10
 
-def findEvents(symbols, startday,endday, marketSymbol,verbose=False):
 
-	# Reading the Data for the list of Symbols.	
-	timeofday=dt.timedelta(hours=16)
-	timestamps = du.getNYSEdays(startday,endday,timeofday)
-	dataobj = da.DataAccess('Yahoo')
-	if verbose:
-            print __name__ + " reading data"
-	# Reading the Data
-	close = dataobj.get_data(timestamps, symbols, closefield)
-	
-	# Completing the Data - Removing the NaN values from the Matrix
-	close = (close.fillna(method='ffill')).fillna(method='backfill')
+def findEvents(symbols, startday, endday, marketSymbol, verbose=False):
+    # Reading the Data for the list of Symbols.
+    timeofday = dt.timedelta(hours=16)
+    timestamps = du.getNYSEdays(startday, endday, timeofday)
+    dataobj = da.DataAccess('Yahoo')
+    if verbose:
+        print __name__ + " reading data"
+    # Reading the Data
+    close = dataobj.get_data(timestamps, symbols, closefield)
 
-	
-	# Calculating Daily Returns for the Market
-	tsu.returnize0(close.values)
-	SPYValues=close[marketSymbol]
+    # Completing the Data - Removing the NaN values from the Matrix
+    close = (close.fillna(method='ffill')).fillna(method='backfill')
 
-	# Calculating the Returns of the Stock Relative to the Market 
-	# So if a Stock went up 5% and the Market rised 3%. The the return relative to market is 2% 
-	mktneutDM = close - close[marketSymbol]
-	np_eventmat = copy.deepcopy(mktneutDM)
-	for sym in symbols:
-		for time in timestamps:
-			np_eventmat[sym][time]=np.NAN
+    # Calculating Daily Returns for the Market
+    tsu.returnize0(close.values)
+    SPYValues = close[marketSymbol]
 
-	if verbose:
-            print __name__ + " finding events"
+    # Calculating the Returns of the Stock Relative to the Market
+    # So if a Stock went up 5% and the Market rised 3%. The the return relative to market is 2%
+    mktneutDM = close - close[marketSymbol]
+    np_eventmat = copy.deepcopy(mktneutDM)
+    for sym in symbols:
+        for time in timestamps:
+            np_eventmat[sym][time] = np.NAN
 
-	# Generating the Event Matrix
-	# Event described is : Market falls more than 3% plus the stock falls 5% more than the Market
-	# Suppose : The market fell 3%, then the stock should fall more than 8% to mark the event.
-	# And if the market falls 5%, then the stock should fall more than 10% to mark the event.
+    if verbose:
+        print __name__ + " finding events"
 
-	for symbol in symbols:
-		
-	    for i in range(1,len(mktneutDM[symbol])):
-	        if SPYValues[i]<-0.03 and mktneutDM[symbol][i] < -0.05 : # When market fall is more than 3% and also the stock compared to market is also fell by more than 5%.
-             		np_eventmat[symbol][i] = 1.0  #overwriting by the bit, marking the event
-			
-	return np_eventmat
+    # Generating the Event Matrix
+    # Event described is : Market falls more than 3% plus the stock falls 5% more than the Market
+    # Suppose : The market fell 3%, then the stock should fall more than 8% to mark the event.
+    # And if the market falls 5%, then the stock should fall more than 10% to mark the event.
+
+    for symbol in symbols:
+
+        for i in range(1, len(mktneutDM[symbol])):
+            if SPYValues[i] < -0.03 and mktneutDM[symbol][
+                i] < -0.05:  # When market fall is more than 3% and also the stock compared to market is also fell by more than 5%.
+                np_eventmat[symbol][
+                    i] = 1.0  # overwriting by the bit, marking the event
+
+    return np_eventmat
 
 
 #################################################
@@ -95,16 +95,19 @@ def findEvents(symbols, startday,endday, marketSymbol,verbose=False):
 #################################################
 
 
-symbols = np.loadtxt('SP500port.csv',dtype='S10',comments='#', skiprows=1)
+symbols = np.loadtxt('SP500port.csv', dtype='S10', comments='#', skiprows=1)
 # You might get a message about some files being missing, don't worry about it.
 
-#symbols =['BFRE','ATCS','RSERF','GDNEF','LAST','ATTUF','JBFCF','CYVA','SPF','XPO','EHECF','TEMO','AOLS','CSNT','REMI','GLRP','AIFLY','BEE','DJRT','CHSTF','AICAF']
-startday = dt.datetime(2008,1,1)
-endday = dt.datetime(2010,12,31)
-eventMatrix = findEvents(symbols,startday,endday,marketSymbol='SPY',verbose=True)
+# symbols =['BFRE','ATCS','RSERF','GDNEF','LAST','ATTUF','JBFCF','CYVA','SPF','XPO','EHECF','TEMO','AOLS','CSNT','REMI','GLRP','AIFLY','BEE','DJRT','CHSTF','AICAF']
+startday = dt.datetime(2008, 1, 1)
+endday = dt.datetime(2010, 12, 31)
+eventMatrix = findEvents(symbols, startday, endday, marketSymbol='SPY',
+                         verbose=True)
 
-eventProfiler = ep.EventProfiler(eventMatrix,startday,endday,lookback_days=20,lookforward_days=20,verbose=True)
+eventProfiler = ep.EventProfiler(eventMatrix, startday, endday,
+                                 lookback_days=20, lookforward_days=20,
+                                 verbose=True)
 
-eventProfiler.study(filename="MyEventStudy.pdf",plotErrorBars=True,plotMarketNeutral=True,plotEvents=False,marketSymbol='SPY')
-
-
+eventProfiler.study(filename="MyEventStudy.pdf", plotErrorBars=True,
+                    plotMarketNeutral=True, plotEvents=False,
+                    marketSymbol='SPY')
